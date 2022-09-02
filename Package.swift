6fdd4ab357d81b650d90d839700dc13387caaa60
wotjd <wotjd971102@gmail.com>
2022-09-03 00:36:46 +0900

    bump iOS min version to 16.0
    for runtime support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CombineReactor",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+        .iOS(.v16), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(name: "CombineReactor", targets: ["CombineReactor"])
